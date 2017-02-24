eacf1fc44ca7b1927b0e4df8aa59116682d36a28
Borut Hollan <borut.hollan@gmail.com>
2017-02-24 12:16:07 +0100

    Composer + serviceprovider

---
+++ src/FoodServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace hollanbo\Food;
+
+use Illuminate\Support\ServiceProvider;
+
+class FoodServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
